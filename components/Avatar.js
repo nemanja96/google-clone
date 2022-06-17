f84d1fa1392c7a177b412606c836a54aff3fd63f
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-06-17 20:55:15 +0200

    Building the Google Front Page - Part 1

---
+++ components/Avatar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image';
+
+const Avatar = ({ url }) => {
+  return (
+    <div className="h-8 w-8 relative cursor-pointer transition duration-150 transform hover:scale-110">
+        <Image loading="lazy" src={url} alt="Profile picture" layout="fill" objectFit="cover" className="rounded-full " />
+    </div>
+  )
+}
+
+export default Avatar
